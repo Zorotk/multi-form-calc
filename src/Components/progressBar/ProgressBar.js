9533908014fa5420e853385b3b21b1998a9057ab
Pavel <zorotkov@gmail.com>
2021-09-25 23:20:53 +0300

    change count

--- src/Components/progressBar/ProgressBar.js
+++ src/Components/progressBar/ProgressBar.js
@@ -1,18 +1,22 @@
-import React, {useState, useEffect, useCallback, useMemo} from 'react';
+import React, {useState, useEffect, useMemo} from 'react';
 import './ProgressBar.scss'
 
 
 export const ProgressBar = ({step}) => {
-
+    const [countStep, setcountStep] = useState(5)
     const [activeStep, setactiveStep] = useState(1)
     useEffect(() => {
         setactiveStep(Number(step))
     }, [step])
 
-    const steps = Array(5).fill(0).map((_, i) => i + 1)
+    const steps = useMemo(() => Array(Number(countStep)).fill(0).map((_, i) => i + 1)
+        , [countStep])
+
 
     return (
         <>
+            <b>Количество шагов</b>
+            <input value={countStep} onChange={(e) => setcountStep(e.target.value)} type="number"/>
             <div className={"steps"}>
                 <div className={'progress'}
                      style={{width: `${(activeStep * (100 / (steps.length - 1))) - 100 / (steps.length - 1)}%`}}/>
